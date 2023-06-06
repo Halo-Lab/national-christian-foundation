3b76902d34b07f7e666a1bce31bdbbb9f28465a6
Ihor Bozhuk <88133512+Jotunn1@users.noreply.github.com>
2023-06-06 18:41:27 +0300

    added filled icons for side panel

--- src/components/PageTag/index.jsx
+++ src/components/PageTag/index.jsx
@@ -1,13 +1,13 @@
 import styles from "./PageTag.module.scss";
 import cn from "classnames";
 
-const PageTag = ({ pageName, icon, isActive, setActivePage }) => {
+const PageTag = ({ pageName, icon, activeIcon, isActive, setActivePage }) => {
     return (
         <li
             className={cn("cta", styles.wrapper, isActive ? styles.active : "")}
             onClick={() => setActivePage(pageName)}
         >
-            <div className={styles.icon}>{icon}</div>
+            <div className={styles.icon}>{isActive ? activeIcon : icon}</div>
             <p className={cn("text-m", isActive && "bold", styles.text)}>
                 {pageName}
             </p>
--- src/components/PagesList/index.jsx
+++ src/components/PagesList/index.jsx
@@ -1,23 +1,29 @@
+import { useState } from "react";
 import PageTag from "../PageTag";
 import styles from "./PagesList.module.scss";
 import Home from "../../assets/icons/Home";
+import HomeFilled from "../../assets/icons/HomeFilled";
 import Grant from "../../assets/icons/Grant";
+import GrantFilled from "../../assets/icons/GrantFilled";
 import Give from "../../assets/icons/Give";
+import GiveFilled from "../../assets/icons/GiveFilled";
 import Favourites from "../../assets/icons/Favourites";
+import FavouritesFilled from "../../assets/icons/FavouritesFilled";
 import Activity from "../../assets/icons/Activity";
+import ActivityFilled from "../../assets/icons/ActivityFilled";
 import Legacy from "../../assets/icons/Legacy";
-import { useState } from "react";
+import LegacyFilled from "../../assets/icons/LegacyFilled";
 
 const PagesList = () => {
     const [activePage, setActivePage] = useState("grant");
 
     const pages = [
-        { name: "home", icon: <Home /> },
-        { name: "grant", icon: <Grant /> },
-        { name: "give", icon: <Give /> },
-        { name: "favourites", icon: <Favourites /> },
-        { name: "activity", icon: <Activity /> },
-        { name: "legacy", icon: <Legacy /> },
+        { name: "home", icon: <Home />, activeIcon: <HomeFilled /> },
+        { name: "grant", icon: <Grant />,activeIcon: <GrantFilled /> },
+        { name: "give", icon: <Give />,activeIcon: <GiveFilled />  },
+        { name: "favourites", icon: <Favourites />,activeIcon: <FavouritesFilled />  },
+        { name: "activity", icon: <Activity />,activeIcon: <ActivityFilled />  },
+        { name: "legacy", icon: <Legacy />,activeIcon: <LegacyFilled />  },
     ];
 
     return (
@@ -27,6 +33,7 @@ const PagesList = () => {
                     <PageTag
                         pageName={page.name}
                         icon={page.icon}
+                        activeIcon={page.activeIcon}
                         isActive={activePage === page.name}
                         setActivePage={setActivePage}
                         key={index}
