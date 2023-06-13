bb90c1f1ba315f645e23ed3dc13242098e83437e
Ihor Bozhuk <88133512+Jotunn1@users.noreply.github.com>
2023-06-13 14:04:41 +0300

    added styles for ghost button type

--- src/pages/GrantPage/index.jsx
+++ src/pages/GrantPage/index.jsx
@@ -3,6 +3,9 @@ import styles from "./GrantPage.module.scss";
 import UserBalanceDisplay from "components/UserBalanceDisplay";
 import SelectCharity from "components/SelectCharity";
 import SuggestedCharities from "components/SuggestedCharities";
+import GrantDetails from "components/GrantDetails";
+import Specialnstructions from "components/Specialnstructions";
+import Button from "components/common/Button";
 
 const GrantPage = () => {
     return (
@@ -11,6 +14,12 @@ const GrantPage = () => {
             <div className={styles.container}>
                 <div className={styles.leftSide}>
                     <SelectCharity />
+                    <GrantDetails />
+                    <Specialnstructions />
+                    <div className={styles.buttons}>
+                        <Button text="Submit grant" type="primary" />
+                        <Button text="Clear all" type="ghost" />
+                    </div>
                 </div>
                 <div className={styles.rightSide}>
                     <UserBalanceDisplay balance={"123,456.00"} />
