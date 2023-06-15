3b95a8fdf38fb90ffae2660f5e0bef9f6a19963a
Ihor Bozhuk <88133512+Jotunn1@users.noreply.github.com>
2023-06-15 12:36:19 +0300

    added controls row to video player

--- src/components/VideoPlayer/index.jsx
+++ src/components/VideoPlayer/index.jsx
@@ -3,10 +3,12 @@ import styles from "./VideoPlayer.module.scss";
 import Pause from "assets/icons/Pause";
 import Play from "assets/icons/Play";
 import cn from "classnames";
+import { transformSecToMin } from "helpers/transformSecToMin";
 
 const VideoPlayer = ({ src }) => {
     const videoRef = useRef(null);
     const [isPlaying, setIsPlaying] = useState(false);
+    const [isMouseOver, setIsMouseOver] = useState(false);
 
     const togglePlay = () => {
         const video = videoRef.current;
@@ -24,26 +26,53 @@ const VideoPlayer = ({ src }) => {
         video.currentTime = seekTime;
     };
 
+    const handleMouseEnter = () => setIsMouseOver(true);
+
+    const handleMouseLeave = () => setIsMouseOver(false);
+
     return (
-        <div className={styles.wrapper}>
-            <video className={styles.video} ref={videoRef} src={src} />
-            <button onClick={togglePlay} className={cn("cta", styles.playBtn)}>
+        <div
+            className={styles.wrapper}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+        >
+            <video
+                className={styles.video}
+                ref={videoRef}
+                src={src}
+                muted
+                loop
+            />
+            <button
+                onClick={togglePlay}
+                className={cn(
+                    "cta",
+                    styles.playBtn,
+                    isMouseOver && styles.active
+                )}
+            >
                 <div className="icon">{isPlaying ? <Pause /> : <Play />}</div>
             </button>
-            {/* <div>
-                <input
-                    type="range"
-                    min="0"
-                    max="100"
-                    step="0.1"
-                    onChange={handleSeek}
-                    value={
-                        (videoRef.current?.currentTime /
-                            videoRef.current?.duration) *
-                            100 || 0
-                    }
-                />
-            </div> */}
+            <div
+                className={cn(styles.controlsRow, isMouseOver && styles.active)}
+            >
+                {/* <div>{transformSecToMin(videoRef.current?.currentTime)}</div> */}
+                <div>
+                    <input
+                        type="range"
+                        min="0"
+                        max="100"
+                        step="0.1"
+                        onChange={handleSeek}
+                        value={
+                            (videoRef.current?.currentTime /
+                                videoRef.current?.duration) *
+                                100 || 0
+                        }
+                    />
+                </div>
+                {/* <div>{transformSecToMin(videoRef.current?.duration)}</div> */}
+            </div>
         </div>
     );
 };
