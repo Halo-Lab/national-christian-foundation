5e582208e452b882b30ddac06ba3f0440e659cfe
Ihor Bozhuk <88133512+Jotunn1@users.noreply.github.com>
2023-06-15 10:49:47 +0300

    created about charity section

---
+++ src/components/AboutCharity/index.jsx
@@ -0,0 +1,44 @@
+import styles from "./AboutCharity.module.scss";
+import cn from "classnames";
+
+const AboutCharity = () => {
+    return (
+        <div className={cn("grant-bg", styles.wrapper)}>
+            <div className={styles.video}>
+                <h4 className="title-h4">Videos block</h4>
+                <p>video</p>
+            </div>
+            <div className={styles.about}>
+                <h4 className="title-h4">About us:</h4>
+                <ul className={styles.list}>
+                    <li>
+                        <p className="text-m">
+                            Lorem Ipsum is simply dummy text
+                        </p>
+                    </li>
+                    <li>
+                        <p className="text-m">
+                            Lorem Ipsum is simply dummy text of the printing and
+                            typesetting industry.
+                        </p>
+                    </li>
+                    <li>
+                        <p className="text-m">
+                            Lorem Ipsum has been the industry's standard dummy
+                            text ever since the 1500s, when an unknown printer
+                            took a galley of type and scrambled it to make a
+                            type specimen book
+                        </p>
+                    </li>
+                    <li>
+                        <p className="text-m">
+                            Lorem Ipsum is simply dummy text
+                        </p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default AboutCharity;
--- src/pages/CharityProfile/index.jsx
+++ src/pages/CharityProfile/index.jsx
@@ -3,6 +3,7 @@ import styles from "./CharityProfile.module.scss";
 import CharityProfileInfo from "components/CharityProfileInfo";
 import CharityContactInfo from "components/CharityContactInfo";
 import CausesList from "components/CausesList";
+import AboutCharity from "components/AboutCharity";
 
 const CharityProfile = () => {
     const { state } = useLocation();
@@ -11,6 +12,7 @@ const CharityProfile = () => {
         <div className={styles.wrapper}>
             <div className={styles.leftSide}>
                 <CharityProfileInfo organisation={state} />
+                <AboutCharity />
             </div>
             <div className={styles.rightSide}>
                 <CharityContactInfo contacts={state.contacts} />
